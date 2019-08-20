46a997227a00433e4ad92ce420c208a4a42a24ab
Carlos Aravena <caravena@rembox.cl>
2019-08-19 20:29:45 -0400

    Configuración

--- devel/www/app/Providers/AuthServiceProvider.php
+++ devel/www/app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'Org\Model' => 'Org\Policies\ModelPolicy',
+        'Org\Model' => 'Org\Policies\ModelPolicy',
     ];
 
     /**
@@ -26,5 +26,23 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        // the gate checks if the user is an admin or a superadmin
+        Gate::define('AdminPanel', function($user) {
+            return $user->hasAnyRole('admin');
+        });
+        Gate::define('AdminSupervisorPanel', function($user) {
+            //return $user->hasAnyRole(['admin','super']);
+            return $user->hasAnyRole(['admin','super']);
+        });
+        Gate::define('SupervisorPanel', function($user) {
+            return $user->hasAnyRole('super');
+        });
+        // the gate checks if the user is a member
+        Gate::define('UsuarioPanel', function($user) {
+            //return $user->role('member');
+            //return $user->authorizeRoles('user');
+            return $user->hasAnyRole('user');
+        });
+
     }
 }
